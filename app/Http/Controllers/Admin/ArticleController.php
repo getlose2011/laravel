d436bcefde398fdda834de0ab5820da623507d38
allen <getlose2011@gmail.com>
2016-05-14 23:58:25 +0800

    RESTful 資源控制器路由(Route::resource)
    可以使用 make:controller Artisan 指令，快速創建控制器 , 接著，我們註冊一個指向此控制器的資源路由 , 可以打
    php artisan route:list 指令查看.
    參考文件
    https://laravel.tw/docs/5.2/controllers#restful-resource-controllers

---
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 仁興
+ * Date: 2016/5/14
+ * Time: 下午 11:47
+ */
+
+namespace App\Http\Controllers\Admin;
+use App\Http\Controllers\Controller;
+
+
+class ArticleController extends Controller
+{
+    public function index(){
+        return '文章首頁get';
+    }
+}
