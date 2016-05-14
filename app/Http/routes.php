b0d53cbad3162779ba3a0f5b21e283173ed7ba58
allen <getlose2011@gmail.com>
2016-05-15 00:02:11 +0800

    在routes.php加入資源控制器路由
    可以跟上個commit一起看.
    RESTful 資源控制器路由(Route::resource)

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -22,6 +22,7 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
     Route::get('login', 'IndexController@login');
     Route::get('index', 'IndexController@index');
+    Route::resource('article', 'ArticleController');//資源控制器路由
 });
 
 ////Named Routes(第一種用法)
