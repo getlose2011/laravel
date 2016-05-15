ba83aaabe084d8ee0491516666088d93d5188e74
allen <getlose2011@gmail.com>
2016-05-15 16:07:19 +0800

    blade模版 , 流程控制
    if else, for, foreach, forelse empty.
    forelse empty好用 , 如沒有變數為空 , 要給預設值@empty

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -10,22 +10,16 @@ class ViewController extends Controller
 {
     //
     public function index(){
-        //如果只有一個參數
-        //$name = "網站";
-        //return view('my_laravel')->with("name",$name);//分配view
-
-//        //如果很多個參數
-//        $data = [
-//            'name' => 'web',
-//            'age' => 12
-//        ];
-//        return view('my_laravel', $data);//分配view
-        //如果有其它不一樣的參數
-//        $data = [
-//            'name' => 'web',
-//            'age' => 12
-//       ];
-        $name = '<script>alert(1)</script>';
-       return view('my_laravel', compact('name'));//分配view
+        $data = [
+            'score' => 60,
+            'number' => 1,
+            'article' =>[
+                'news 1',
+                'news 2',
+                'news 4',
+            ],
+            'news' => array(),
+        ];
+        return view('my_laravel', compact('data'));
     }
 }
--- resources/views/my_laravel.blade.php
+++ resources/views/my_laravel.blade.php
@@ -36,13 +36,25 @@
     <body>
         <div class="container">
             <div class="content">
-                <!--
-                1. @{{ 變數 }}直接印出來
-                2. 如果變數$name 是null 要有預設值則可以這樣寫{{$name or 'xxx'}}
-                3. 如果判斷是有這變數可以用 {{isset($name)?$name:'xxx'}}
-                4. 要執行javascript腳本則要寫成{!! $name !!} ; 如果寫成{{$name}} , 則會被實體化 , 直接打印在網頁上 , 防止腳本被運行
-                 -->
-                <div class="title">ing</div>
+                @if($data['score'] < 60)
+                    不及格<br />
+                @else
+                    及格<br />
+                @endif
+
+                @for($i=1; $i < 10; $i++)
+                    {{$i}}<br />
+                @endfor
+
+                @foreach($data['article'] as $d)
+                    {{$d}}<br />
+                 @endforeach
+
+                 @forelse($data['news'] as $d)
+                        {{$d}}<br />
+                 @empty
+                        no data
+                 @endforelse
             </div>
         </div>
     </body>
